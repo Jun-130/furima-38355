d62590087f7659e2f7e67d28a49be2eaa0b7da16
jun-130 <happy.smile.p0p0@gmail.com>
2022-09-06 17:32:35 +0900

    validationの設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,19 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  validates_format_of :password, with: PASSWORD_REGEX
+
+  with_options presence: true do
+    validates birth_date
+    with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
+      validates :last_name
+      validates :first_name
+    end
+    with_options format: {with: /\A[ァ-ヶー]+\z/} do
+      validates :kana_last_name
+      validates :kana_first_name
+    end
+  end
 end
