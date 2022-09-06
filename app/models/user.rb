94669c64fc777f6d54cb26acfffc36c13034f634
jun-130 <happy.smile.p0p0@gmail.com>
2022-09-06 18:00:29 +0900

    カラム名の変更
    カラム名の変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,7 @@ class User < ApplicationRecord
   validates_format_of :password, with: PASSWORD_REGEX
 
   with_options presence: true do
-    validates birth_date
+    validates :birth_date
     with_options format: {with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/} do
       validates :last_name
       validates :first_name
