90048c478524525c2ad4c3a0f7746800c5e37940
jun-130 <happy.smile.p0p0@gmail.com>
2022-09-07 06:24:58 +0900

    Rubocop_check

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,19 +4,20 @@ class User < ApplicationRecord
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 
-  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z/i.freeze
+  PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   full_width = /\A[ぁ-んァ-ヶ一-龥々ー]+\z/
   full_width_kana = /\A[ァ-ヶー]+\z/
 
-  validates_format_of :password, {with: PASSWORD_REGEX, messege: "is invalid. Include both letters and numbers"}
+  validates_format_of :password, { with: PASSWORD_REGEX, messege: "is invalid. Include both letters and numbers" }
 
   with_options presence: true do
+    validates :nickname
     validates :birth_date
-    with_options format: {with: full_width, message: "is invalid. Input full-width characters"} do
+    with_options format: { with: full_width, message: "is invalid. Input full-width characters" } do
       validates :last_name
       validates :first_name
     end
-    with_options format: {with: full_width_kana, message: "is invalid. Input full-width katakana characters"} do
+    with_options format: { with: full_width_kana, message: "is invalid. Input full-width katakana characters" } do
       validates :kana_last_name
       validates :kana_first_name
     end
