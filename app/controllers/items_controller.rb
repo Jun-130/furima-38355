b7deaa0d6fb4c785c76106386dfe4065303b7356
jun-130 <happy.smile.p0p0@gmail.com>
2022-09-07 20:44:32 +0900

    出品機能の実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,11 +1,26 @@
 class ItemsController < ApplicationController
   def index
-    @item = Item.new
   end
 
   def new
+    @item = Item.new
   end
 
   def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to root_path
+    else
+      render :new
+    end 
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(
+      :image, :name, :description, :price, :category_id, :condition_id,
+      :shipping_charge_id, :prefecture_id, :shipping_day_id)
+      .merge(user_id: current_user.id)
   end
 end
