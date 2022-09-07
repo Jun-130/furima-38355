9c4f5d3a8742e5ef2cb1fb940482a2bb38829ce3
jun-130 <happy.smile.p0p0@gmail.com>
2022-09-07 19:30:21 +0900

    price_validationの追加

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,14 +1,16 @@
 class Item < ApplicationRecord
   belongs_to :user
 
+  half_width_digit = /\A[0-9]+\z/
+
   with_options presence: true do
     validates :name
     validates :description
-    validates :price
     validates :category_id
     validates :condition_id
     validates :shipping_charge_id
     validates :prefecture_id
     validates :shipping_day_id
+    validates :price, format: { with: half_width_digit }, numericality: { in: 300..9_999_999 }
   end
 end
