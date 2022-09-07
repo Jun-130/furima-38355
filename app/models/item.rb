cd811b7e5ec5fa45fd42e54b92dda0326465e893
jun-130 <happy.smile.p0p0@gmail.com>
2022-09-07 18:56:48 +0900

    validationの設定

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,3 +1,14 @@
 class Item < ApplicationRecord
   belongs_to :user
+
+  with_options presence: true do
+    validates :name
+    validates :description
+    validates :price
+    validates :category_id
+    validates :condition_id
+    validates :shipping_charge_id
+    validates :prefecture_id
+    validates :shipping_day_id
+  end
 end
