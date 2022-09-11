565d2568fa627d296b23b74c0bde15cc12660523
jun-130 <happy.smile.p0p0@gmail.com>
2022-09-11 14:48:35 +0900

    order保存機能の実装

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,18 +1,23 @@
 class OrderAddress
 
   include ActiveModel::Model
-  attr_accessor :zip_code, :prefecture_id, :city, :street, :building, :phone_number, :order_id, :user_id, :item_id
+  attr_accessor :zip_code, :prefecture_id, :city, :street, :building, :phone_number, :user_id, :item_id
 
   digit3_digit4 = /\A\d{3}-\d{4}\z/
 
   with_options presence: true do
-    validates :zip_code, format: {with: digit3_digit4, message: "is invalid. Include hyphen(-)"}
+    validates :zip_code, format: { with: digit3_digit4, message: "is invalid. Enter it as follows (e.g. 123-4567)" }
     validates :prefecture_id
     validates :city
     validates :street
-    validates :phone_number
-    validates :order_id
+    validates :phone_number, numericality: { only_integer: true, message: "is invalid. Input only number" },
+                             length: { minimum: 10, message: "is too short" }
     validates :user_id
     validates :item_id
+  end
+
+  def save
+    order = Order.create(user_id: user_id, item_id: item_id)
+    Address.create(zip_code: zip_code, prefecture_id: prefecture_id, city: city, street: street, building: building, phone_number: phone_number, order_id: order.id)
   end
 end
